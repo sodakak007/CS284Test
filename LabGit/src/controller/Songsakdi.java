16245982ffa0af196d7fa46eba5ade3d72d56469
Soda <sodawongwaran@gmail.com>
2017-11-10 12:47:59 +0700

    code อะไรบางอย่าง ที่สามารถรันได้

---
+++ LabGit/src/controller/Songsakdi.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javax.swing.JOptionPane;
+
+public class Songsakdi {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Welcome to Github Lab.");
+		String a=JOptionPane.showInputDialog("What??????");
+		System.out.println(a);
+	}
+
+}
